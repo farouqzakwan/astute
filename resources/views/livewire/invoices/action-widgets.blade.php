9d0c47310eef7b1a81aa1d222351e60c437f42b1
farouqzakwan <farouqzakwan@gmail.com>
2021-11-07 12:03:18 +0800

    refactor: create component for actions
    - create livewire components for widgets print and preview

---
+++ resources/views/livewire/invoices/action-widgets.blade.php
@@ -0,0 +1,24 @@
+<div>
+    
+    <div class="w-full mt-8 bg-white dark:bg-gray-800 py-5 px-3 flex flex-row-reverse">
+        <a 
+            target="_blank"
+            href="{{ route('dashboard.invoices.print') }}" 
+            class="flex items-center justify-left px-4 py-2 mx-3 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+            </svg>
+            <span>Preview</span>
+        </a>
+        <a 
+            target="_blank"
+            href="{{ route('dashboard.invoices.preview') }}" 
+            class="flex items-center justify-left px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z" />
+            </svg>
+            <span>Print</span>
+        </a>
+    </div>  
+</div>
