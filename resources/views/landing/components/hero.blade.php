abc53123eca7fd5c1a11ff43807c0b4ce9ac6dcc
farouqzakwan <farouqzakwan@gmail.com>
2022-02-27 11:37:29 +0800

    update hero section
    Updating hero sections

--- resources/views/landing/components/hero.blade.php
+++ resources/views/landing/components/hero.blade.php
@@ -1,16 +1,10 @@
-<section class="text-gray-600 body-font">
+<section class="text-black body-font bg-gradient-to-r from-blue-300 to-indigo-800">
     <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
       <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-        <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Knausgaard typewriter readymade marfa</h1>
-        <p class="mb-8 leading-relaxed">Chillwave portland ugh, knausgaard fam polaroid iPhone. Man braid swag typewriter affogato, hella selvage wolf narwhal dreamcatcher.</p>
-        <div class="flex w-full md:justify-start justify-center items-end">
-          <div class="relative mr-4 md:w-full lg:w-full xl:w-1/2 w-2/4">
-            <label for="hero-field" class="leading-7 text-sm text-gray-600">Placeholder</label>
-            <input type="text" id="hero-field" name="hero-field" class="w-full bg-gray-100 rounded border bg-opacity-50 border-gray-300 focus:ring-2 focus:ring-green-200 focus:bg-transparent focus:border-green-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-          </div>
-          <button class="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">Button</button>
-        </div>
-        <p class="text-sm mt-2 text-gray-500 mb-8 w-full">Neutra shabby chic ramps, viral fixie.</p>
+        <h1 class="title-font sm:text-4xl text-3xl mb-4 font-extrabold text-white">Create professional invoice for free</h1>
+        <p class="mb-8 leading-relaxed">Every invoice paid is positive incoming revenue for your business. {{ config('app.name') }} invoice software enables you to easily create and send professional invoices for free.</p>
+    
+        <p class="text-sm mt-2 text-black mb-8 w-full">{{ config('app.name') }} is now available on mobile</p>
         <div class="flex lg:flex-row md:flex-col">
           <button class="bg-gray-100 inline-flex py-3 px-5 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
             <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-6 h-6" viewBox="0 0 512 512">
@@ -34,7 +28,7 @@
         </div>
       </div>
       <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-        <img class="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600">
+        <img class="object-cover object-center rounded" alt="hero" src="{{ url('/image/home/invoice.png') }}">
       </div>
     </div>
 </section>
